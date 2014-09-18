f347b73da748350eec60c2a7ad6c546931cf8fe2
Jonas <jonas.scheiwiller@zhdk.ch>
2014-09-18 10:22:01 +0200

    digitalWrite fade with microsec delay

--- arduino/.build/uno/src/sketch.cpp
+++ arduino/.build/uno/src/sketch.cpp
@@ -1,39 +1,37 @@
 #include <Arduino.h>
-#include <math.h>
+
 void setup();
 void loop();
 #line 1 "src/sketch.ino"
-//#include <math.h>
-
-int led = 10;           // the pin that the LED is attached to
-int fadeAmount = 1;    // how many points to fade the LED by
-int fadeAmount2 = 1;
-int durationOn = 100;
-int durationOff = 100;
-int roundedOn;
-int roundedOff;
-
-void setup()  { 
-  pinMode(led, OUTPUT);
-
-} 
-
-// the loop routine runs over and over again forever:
-void loop()  { 
-  digitalWrite(led, HIGH);
-  roundedOn = round(durationOn);
-  delay(roundedOn/100);
-  digitalWrite(led, LOW);
-  roundedOff = round(durationOff);
-  delay(roundedOff/100);
-
-  durationOn = durationOn + fadeAmount;
-  durationOff = durationOff - fadeAmount2;
-
-
-  if (durationOn == 0 || durationOn == 600) {
-    fadeAmount = -fadeAmount ; 
-  } else if (durationOff == 0 || durationOff == 600){
-    fadeAmount2 = -fadeAmount2;
-  }    
+int ledPin = 10;
+unsigned int i = 0;
+boolean rise = true;
+int period = 1000;
+void setup()
+{
+    pinMode(ledPin, OUTPUT);
+}
+void loop()
+{
+    if (i == period)
+    {
+        i = 1;
+        rise = !rise;
+    }
+    if (rise == false)
+    {
+        digitalWrite(ledPin, LOW);
+        delayMicroseconds(i);
+        digitalWrite(ledPin, HIGH);
+        delayMicroseconds(period - i);
+        i = i + 1;
+    }
+    if (rise == true)
+    {
+        digitalWrite(ledPin, LOW);
+        delayMicroseconds(period - i);
+        digitalWrite(ledPin, HIGH);
+        delayMicroseconds(i);
+        i = i + 1;
+    }
 }
--- arduino/src/sketch.ino
+++ arduino/src/sketch.ino
@@ -1,34 +1,35 @@
-#include <math.h>
+// Code from http://www.rngtng.com/2009/05/17/arduino-fade-and-pulse-a-led-with-just-using-a-digitalport/
+//digitalWrite fading
 
-int led = 10;           // the pin that the LED is attached to
-int fadeAmount = 1;    // how many points to fade the LED by
-int fadeAmount2 = 1;
-int durationOn = 100;
-int durationOff = 100;
-int roundedOn;
-int roundedOff;
-
-void setup()  { 
-  pinMode(led, OUTPUT);
-
-} 
-
-// the loop routine runs over and over again forever:
-void loop()  { 
-  digitalWrite(led, HIGH);
-  roundedOn = round(durationOn);
-  delay(roundedOn/100);
-  digitalWrite(led, LOW);
-  roundedOff = round(durationOff);
-  delay(roundedOff/100);
-
-  durationOn = durationOn + fadeAmount;
-  durationOff = durationOff - fadeAmount2;
-
-
-  if (durationOn == 0 || durationOn == 600) {
-    fadeAmount = -fadeAmount ; 
-  } else if (durationOff == 0 || durationOff == 600){
-    fadeAmount2 = -fadeAmount2;
-  }    
+int ledPin = 10;
+unsigned int i = 0;
+boolean rise = true;
+int period = 1000;
+void setup()
+{
+    pinMode(ledPin, OUTPUT);
+}
+void loop()
+{
+    if (i == period)
+    {
+        i = 1; //reset counter
+        rise = !rise; //change rise to contrary
+    }
+    if (rise == false)
+    {
+        digitalWrite(ledPin, LOW);
+        delayMicroseconds(i);
+        digitalWrite(ledPin, HIGH);
+        delayMicroseconds(period - i);
+        i = i + 1; //prolong delay i
+    }
+    if (rise == true)
+    {
+        digitalWrite(ledPin, LOW);
+        delayMicroseconds(period - i);
+        digitalWrite(ledPin, HIGH);
+        delayMicroseconds(i);
+        i = i + 1;
+    }
 }
