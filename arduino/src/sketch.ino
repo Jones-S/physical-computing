38e2aeaf1a5c8fa6e3ad0f235c549e9df8e807c7
Jonas <jonas.scheiwiller@zhdk.ch>
2014-09-18 10:17:08 +0200

    crippled digitalWrite fade. not working properly

--- arduino/src/sketch.ino
+++ arduino/src/sketch.ino
@@ -1,6 +1,12 @@
+#include <math.h>
+
 int led = 10;           // the pin that the LED is attached to
-int fadeAmount = 5;    // how many points to fade the LED by
-int duration = 10;
+int fadeAmount = 1;    // how many points to fade the LED by
+int fadeAmount2 = 1;
+int durationOn = 100;
+int durationOff = 100;
+int roundedOn;
+int roundedOff;
 
 void setup()  { 
   pinMode(led, OUTPUT);
@@ -10,12 +16,19 @@ void setup()  {
 // the loop routine runs over and over again forever:
 void loop()  { 
   digitalWrite(led, HIGH);
-  delay(duration);
-  digitalWrite(led, LOW)
+  roundedOn = round(durationOn);
+  delay(roundedOn/100);
+  digitalWrite(led, LOW);
+  roundedOff = round(durationOff);
+  delay(roundedOff/100);
 
-  duration = duration + fadeAmount;
+  durationOn = durationOn + fadeAmount;
+  durationOff = durationOff - fadeAmount2;
 
-  if (duration == 0 || duration == 1000) {
+
+  if (durationOn == 0 || durationOn == 600) {
     fadeAmount = -fadeAmount ; 
-  }     
+  } else if (durationOff == 0 || durationOff == 600){
+    fadeAmount2 = -fadeAmount2;
+  }    
 }
