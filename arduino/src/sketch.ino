b2353754a43bd983aeb95dd4493432d1e2f6ee9d
Jonas <jonas.scheiwiller@zhdk.ch>
2014-09-18 16:18:17 +0200

    parrot with duration of pauses and pressTime implemented, but not working properly

--- arduino/src/sketch.ino
+++ arduino/src/sketch.ino
@@ -13,7 +13,7 @@ int lastClick = 0;
 int click;
 int pressCounter = 0;
 
-int parrotStorage[20];
+int parrotStorage[20][2]; // [][0] = duration, [][1] = LOW/HIGH
 
 void setup()
 {
@@ -40,23 +40,18 @@ void loop()
 
 
 
-    if (change && actualState)  //if button is down and a change happened
+    if (change)  //if button is down and a change happened
     {
         change = false;
         // if long enough start time
-        start = millis(); //save current time in start
-    }
-    else if (change && !actualState) //if button up and change happened
-    {
-        change = false;
-        duration = millis() - start; //save duration by subtracting start from millis
+        duration = millis() - lastClick; //save duration by subtracting lastClick from millis
 
         //check if button was pressed for a satisfying duration
         if (duration > minimumTime)
         {
             //save duration in array
-            parrotStorage[pressCounter] = duration;
-            Serial.println(parrotStorage[pressCounter]);
+            parrotStorage[pressCounter][0] = duration;
+            parrotStorage[pressCounter][1] = !actualState;
             pressCounter++;
         }
         else
@@ -72,15 +67,18 @@ void loop()
         //stop recording and play it
         for (int i = 0; i < pressCounter; i++)
         {
-            digitalWrite(led, HIGH);
-            delay(parrotStorage[i]);
-            digitalWrite(led, LOW);
-            delay(400);
-            parrotStorage[i] = 0;
+            digitalWrite(led, parrotStorage[i][1]);
+            delay(parrotStorage[i][0]);
+            parrotStorage[i][0] = 0;
+            parrotStorage[i][1] = 0;
 
         }
 
-        lastClick, click, pressCounter = 0;
+        lastClick = 0;
+        click = 0; 
+        pressCounter = 0;
+        duration = 0;
+        digitalWrite(led, LOW);
 
     }
 
