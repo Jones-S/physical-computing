1ecc531d0c28349f94b49981d39cc07e2895f813
Jonas <jonas.scheiwiller@zhdk.ch>
2014-09-30 10:35:28 +0200

    servo control via potentiometer

--- arduino/servo/src/sketch.ino
+++ arduino/servo/src/sketch.ino
@@ -1,21 +1,26 @@
 #include <Servo.h>
- 
+#define INPUT_PIN 5
 Servo myservo;
+
+int inputVal = 0;
  
 void setup()
 {
   myservo.attach(9);
   myservo.write(0);
+  Serial.begin(9600);
 }
  
 void loop()
 {
+  inputVal = analogRead(INPUT_PIN);
+  //map inputVal to degree
+  int mappedVal = map(inputVal, 0, 1023, 0, 180);
+
+
+  myservo.write(mappedVal);
   delay(1000);
-  myservo.write(90);
-  delay(1000);
-  myservo.write(180);
-  delay(1000);
-  myservo.write(90);
-  delay(1000);
-  myservo.write(0);
+
+  Serial.println(inputVal);
+  
 }
