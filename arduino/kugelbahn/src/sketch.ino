01c935702dd7d5696881d0f9562c9d7930165bee
Jonas <jonas.scheiwiller@zhdk.ch>
2014-10-08 01:22:52 +0200

    prepare kugelbahn arduino sketch (added debounce for saving the lastButton events and initiating all necessary variables)

--- arduino/kugelbahn/src/sketch.ino
+++ arduino/kugelbahn/src/sketch.ino
@@ -1,49 +1,112 @@
-#define INPUT_COIL A1
+#define INPUT_INDUCTOR A1
 #define INPUT_BUT1 3
 #define INPUT_BUT2 5
 #define LED 9
 
-int lastSignal = 0; //will hold the time in ms when the last signal happened
-int lastButton = 0; //will hold the time in ms when the button was pressed
-int timeBetween = 0;
-int inputCoil = 0;
 
-boolean buttonState = false;
+int inputInductor = 0;
+
+boolean buttonState1;             // the current reading from the input pin
+boolean buttonState2;             // the current reading from the input pin
+boolean lastButtonState1 = LOW;   // the previous reading from the input pin
+boolean lastButtonState2 = LOW;   // the previous reading from the input pin
+
+//booleans for saving fails
+boolean fail1 = 0;
+boolean fail2 = 0;
+
+// the following variables are long's because the time, measured in miliseconds,
+// will quickly become a bigger number than can be stored in an int.
+long lastDebounceTime1 = 0;     // the last time the input button 1 was toggled
+long lastDebounceTime2 = 0;     // the last time the input button 2 was toggled
+long debounceDelay = 50;        // the debounce time; increase if the output flickers
+
+long lastButtonSignal1 = 0;     // vars for saving the time of the last button press-event
+long lastButtonSignal2 = 0;
+long lastSignal = 0;            // var for saving time of last inductor signal
+int timeDifference = 0;         // var for saving the difference in time between newSignal and lastButtonSignal
 
 
-void setup(){
-  //start serial connection
-  Serial.begin(9600);
-  pinMode(INPUT_COIL, INPUT);
-  pinMode(INPUT_BUT1, INPUT_PULLUP);
-  pinMode(LED, OUTPUT);
+void setup() {
+    //start serial connection
+    Serial.begin(9600);
+    pinMode(INPUT_INDUCTOR, INPUT);
+    pinMode(INPUT_BUT1, INPUT_PULLUP);
+    pinMode(LED, OUTPUT);
 
 }
 
-void loop(){
-  inputCoil = analogRead(INPUT_COIL);
-  buttonState = digitalRead(INPUT_BUT1);
-  buttonState = !buttonState;
-  // Serial.println(inputCoil);
-  // Serial.println(buttonState);
+void loop() {
+    //read analog input (0 - 1023) from copper inductor
+    inputInductor = analogRead(INPUT_INDUCTOR);
 
-  if(inputCoil > 300){
-  	lastSignal = millis();
-  	Serial.print(inputCoil);
-  	Serial.print(" ################################");
-  	Serial.println();
-  }
+    // read the state of the switch into a local variable:
+    int reading1 = !digitalRead(INPUT_BUT1); //reverse value because of pullup resistor: 0 if button is pressed
+    int reading2 = !digitalRead(INPUT_BUT2); //reverse value because of pullup resistor: 0 if button is pressed
 
-  if(buttonState){
-  	lastButton = millis();
-  }
 
-  if(lastSignal && lastButton && lastButton > lastSignal){
-  	timeBetween = lastButton - lastSignal;
-  	Serial.println(timeBetween);
-  }
+    //TODO: check highest peak signal
+    if (inputInductor > 300) {
+        lastSignal = millis();
+        newSignal = true;
+        Serial.print(inputInductor);
+        Serial.print(" ################################");
+        Serial.println();
+    }
 
-  digitalWrite(LED, buttonState);
 
+
+    // check to see if you just pressed the button
+    // (i.e. the input went from LOW to HIGH),  and you've waited
+    // long enough since the last press to ignore any noise:
+
+    // If the switch changed, due to noise or pressing:
+    if (reading1 != lastButtonState1) {
+        // reset the debouncing timer
+        lastDebounceTime1 = millis();
+    }
+    //same for switch 2
+    if (reading2 != lastButtonState2) {
+        // reset the debouncing timer
+        lastDebounceTime2 = millis();
+    }
+
+    if ((millis() - lastDebounceTime1) > debounceDelay) {
+        // whatever the reading is at, it's been there for longer
+        // than the debounce delay, so take it as the actual current state:
+
+        // if the button state has changed:
+        if (reading1 != buttonState1) {
+            buttonState1 = reading1; //change current state to (e.g.) HIGH
+        }
+        //check if button is HIGH -> save time to lastButtonSignal
+        if (buttonState1) {
+            lastButtonSignal1 = millis();
+        }
+    }
+    //and same for switch2
+    if ((millis() - lastDebounceTime2) > debounceDelay) {
+        if (reading2 != buttonState2) {
+            buttonState2 = reading2;
+        }
+        if (buttonState2) {
+            lastButtonSignal2 = millis();
+        }
+    }
+
+    //check if evaluation is necessary
+    if(newSignal && lastButtonSignal1 > 0 && !fail1){
+        evaluate ()
+    }
+
+    // save the reading.  Next time through the loop,
+    // it'll be the lastButtonState:
+    lastButtonState1 = reading1;
+    lastButtonState2 = reading2;
+}
+
+
+//evaluation of time difference
+int evaluate () {
 
 }
